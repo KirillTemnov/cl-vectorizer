54c5c71a7e975747e9381db3d6e4b9489fd481d6
selead <allselead@gmail.com>
2010-06-27 01:56:05 +0400

    Refactor module, add objects svg-image and svg-point.

--- src/svg.lisp
+++ src/svg.lisp
@@ -3,13 +3,30 @@
 
 (defclass svg-object nil
     ((stroke       :initarg :stroke       :initform "black")
-     (stroke-width :initarg :stroke-width :initform "1")
+     (stroke-width :initarg :stroke-width :initform 1)
      (id           :initarg :id           :initform nil)
      (object-type  :initarg :object-type  :initform nil))
   (:documentation "Common svg object."))
 
 (defgeneric to-svg (object)
   (:documentation "Transform object to svg-format representation"))
+;;------------------------------------------------------------------------------
+(defclass svg-point (svg-object)
+  ((point       :initarg :point :initform '(0 0))
+   (object-type :initform "point"))
+  (:documentation "Point svg object."))
+
+(defun make-svg-point (point &key (color "black"))
+  "Point is just a list of 2 coordinates."
+  (make-instance 'svg-point :point point :stroke color :stroke-width 2))
+
+(defmethod to-svg ((object svg-point))
+  (let* ((id (cond ((eq nil (slot-value object 'id)) "")
+		   (t (format nil "id=\"~a\" " (slot-value object 'id)))))
+	 (pt (slot-value object 'point))
+	 (x1 (first pt))
+	 (y1 (second pt)))
+    (format nil "<line ~a x1=\"~a\" y1=\"~a\" x2=\"~a\" y2=\"~a\" stroke=\"~a\" stroke-width=\"~a\"/>~%" id x1 y1 (1+ x1) (1+ y1)  (slot-value object 'stroke) (slot-value object 'stroke-width))))
 ;;------------------------------------------------------------------------------
 (defclass svg-line (svg-object)
   ((point1      :initarg :point1      :initform '(0 0))
@@ -65,8 +82,24 @@ Format:
 	 (radius (slot-value object 'radius)))
     (format nil "<circle ~a cx=\"~a\" cy=\"~a\" r=\"~a\" stroke=\"~a\" stroke-width=\"~a\"  fill=\"~a\" fill-opacity=\"~a\"/>~%" 
 	    id (first center) (second center) radius (slot-value object 'stroke) (slot-value object 'stroke-width) (slot-value object 'fill) (slot-value object 'fill-opacity))))
-	 
-		   
+	       ;;------------------------------------------------------------------------------  
+(defclass svg-image (svg-object)
+  ((left-point   :initarg  :left-point :initform '(0 0))
+   (filename     :initarg  :filename   :initform nil)
+   (width        :initarg  :width      :initform "100%")
+   (height       :initarg  :height     :initform "100%")
+   (object-type   :initform "image"))
+  (:documentation "Raster image placed on svg."))
+
+(defmethod to-svg ((object svg-image))
+  (let* ((id (cond ((eq nil (slot-value object 'id)) "")
+		   (t (format nil "id=\"~a\" " (slot-value object 'id)))))
+	 (left (slot-value object 'left-point))
+	 (x (first left))
+	 (y (second left)))
+    (format nil "<image ~a xlink:href=\"~a\" x=\"~a\" y=\"~a\" width=\"~a\" height=\"~a\"/>~%"
+	    id x y (slot-value object 'width) (slot-value object 'height))))
+
 ;;------------------------------------------------------------------------------  
 
 (defun render-svg-file (entities-list width height)
@@ -126,27 +159,41 @@ file-to-save is not resets!"))
 		   (namestring (slot-value manager 'file-to-save)))))
 
 ;;--------------------------------------------------------------------------------
-(defun save-hashtable-lines-as-svg (ht width height &key (filename #p"out.svg"))
-  (let ((mgr (create-svg-manager  width height :filename filename))
-	line
-	(lines 0))
+(defun hashtable-lines-to-svg-manager (ht manager)
+  "Save lines from hash keys to svg manager."
+  (let (line (lines 0))
+
     (loop for point being the hash-key of ht do
 	 (setf line (gethash point ht))
 	 (when (line? line)
-;;	   (print-line line)
 	   (incf lines)
-	   (add-entity mgr (make-svg-line line :color "green"))))
-    (when (get-debug-mode)
-      (format t "Total ~a lines pushed to svg.~%" lines))
-    (flush-manager mgr)))
+	   (add-entity manager (make-svg-line line :color "green"))))
 
-(defun save-hashtable-circles-as-svg (circles width height &key (filename #p"circles.svg"))
-  (let ((mgr (create-svg-manager  width height :filename filename)))
-	(loop for circle being the hash-key of circles do
-	   (add-entity mgr (make-svg-circle circle :color "blue" :width 2)))
-	(when (get-debug-mode)
-	  (format t "Total ~a circles pushed to svg.~%" (hash-table-count circles)))
-	(flush-manager mgr)))
+    (when (get-debug-mode)
+      (format t "Total ~a lines pushed to svg manager.~%" lines))
+    manager))
+
+(defun hashtable-circles-to-svg-manager (circles manager)
+  "Save circles from hash keys to svg manager."
+  (loop for circle being the hash-key of circles do
+       (add-entity manager (make-svg-circle circle :color "blue" :width 2)))
+  (when (get-debug-mode)
+    (format t "Total ~a circles pushed to svg manager.~%" (hash-table-count circles)))
+  manager)
+
+(defun list-points-to-svg-manager (points-list manager)
+  "Save points from list to svg manager."
+  (dolist (pt points-list)
+    (add-entity manager (make-svg-point :point pt)))
+  (when (get-debug-mode)
+    (format nil "Total ~a points pushed to svg manager.~%" (length points-list)))
+  manager)
+
+
+(defun save-svg (manager &key (filename #p"out.svg"))
+  "Save manager to svg file."
+  (setf (slot-value manager 'file-to-save) filename)
+  (flush-manager manager))
 
 				 
 
