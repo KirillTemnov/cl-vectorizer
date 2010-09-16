9c573207ed7740a68b7a746fe6cd6198b10065f8
selead <allselead@gmail.com>
2010-09-16 16:18:17 +0400

    Add circles extraction facilities, not testsd yet.

--- src/histogram.lisp
+++ src/histogram.lisp
@@ -37,15 +37,17 @@ Return list with x-histogram and y-histogram in order."
         (y-histogram (normalize-histogram (second xy-histograms))))
     (list x-histogram y-histogram)))
 
-(defun find-peaks (histogram  &key (min-peak-value 30) (min-peak-distance 4))
+(defun find-peaks (histogram  &key (min-peak-value 30) (offset 0)
+                   (min-peak-distance 4))
   "Find peaks on HISTOGRAM that higher than MIN-PEAK-VALUE.
 If two peaks stand from each other closer than MIN-PEAK-DISTANCE, they
-merged into one peak, which placed between them."
+merged into one peak, which placed between them. OFFSET parameter uses for
+skip several first and last points on histogram. "
   (let ((peaks (make-sequence 'vector (length histogram) :initial-element 0))
         (prev-peak-value 0)
         (prev-peak-distance 0)
         peak)
-    (loop for i from 0 to (1- (length histogram)) do
+    (loop for i from offset to (- (length histogram) offset 1) do
          (setf peak (aref histogram i))
          (when (< min-peak-value peak)
            (if (< prev-peak-distance min-peak-distance)
@@ -65,6 +67,86 @@ merged into one peak, which placed between them."
          (incf prev-peak-distance))
     peaks))
 
+
+(defun find-possibly-circles (points-list &key (min-radius 10))
+  "Find objects like cirlces on image represented by POINTS-LIST (black points)."
+  (labels ((get-symmetric-index (peaks center-index left-index min-value)
+             "Get right-side index of from the CENTER-INDEX and LEFT-INDEX to the right using
+peaks vector. Right-side index is a peak, which is greater or equal than min-value.
+If there aren't any peaks on the estimated distance function returns nil. "
+             (let* ((delta (- center-index left-index))
+                    (max-radius-error 3)       ; error in determining the radius
+                    (ind (- (+ center-index delta) max-radius-error))
+                    right-index)
+               (loop while (and
+                            (< ind (length peaks))
+                            (< ind (+ center-index delta max-radius-error))) do
+                    (when (< min-value (aref peaks ind))
+                      (setf min-value (aref peaks ind))
+                      (setf right-index ind))
+                    (incf ind))
+               right-index))
+
+           (add-circle (circle circles &key (max-radius-error 3))
+             "Add new CIRCLE to CIRCLES only if in CIRCLES not present another circle with
+center equals to CIRCLE's radius and radius equals to CIRCLE's radius + or - MAX-RADIUS-ERROR."
+             (let ((may-add t))
+               (dolist (c circles)
+                 (when (and
+                        (= (getf c :center) (getf circle :center))
+                        (<= (abs (- (getf c :radius) (getf circle :radius))) max-radius-error))
+                   (setf may-add nil)))
+               (when (not (null may-add))
+                 (push circle circles)))
+             circles)
+
+           (generate-circles (x-cirles y-circles &key (max-radius-error 3))
+             "Compare circles radiuses. If the difference between two radiuses is less
+Than MAX-RADIUS-ERROR, add new circle to list."
+             (let (circles-list)
+               (dolist (cx x-cirles)
+                 (dolist (cy y-circles)
+                   (when (<= (abs (- (getf cx :radius) (getf cy :radius))) max-radius-error)
+                     (push (list (list (getf cx :center) (getf cy :center))
+                                 (floor (max (getf cx :radius) (getf cy :radius))))
+                           circles-list))))
+               circles-list))
+
+           (wave (histogram &key (min-peak-value 7)) ; min % of peak, that belongs to a cirle
+             (let* ((peaks (find-peaks histogram))
+                    peak-indexes
+                    possibly-circles)
+               (loop for i from min-radius to (- (length peaks) min-radius 1) do
+                    (when (< 0 (aref peaks i))
+                      (push i peak-indexes)))
+               (nreverse peak-indexes)
+               (setf peaks (find-peaks histogram :min-peak-value min-peak-value))
+
+               (dolist (ind peak-indexes)
+                 (let ((l-index (- ind min-radius)) r-index)
+                   (loop while (< 0 l-index)   do     ; move to begin of histogram
+                        (when (< min-peak-value (aref peaks l-index))
+                          ;; search simmetric peak
+                          (setf r-index (get-symmetric-index peaks ind l-index min-peak-value))
+                          (unless (null r-index)
+                            (setf possibly-circles
+                                  (add-circle (list :center ind
+                                                    :radius (/ (- r-index l-index) 2.0)
+                                                    :center-value (aref peaks ind)
+                                                    :left-side-value (aref peaks l-index))
+                                              possibly-circles))))
+                        (decf l-index))))
+               possibly-circles)))
+
+    (let* ((xy-histograms (make-normalised-histograms points-list))
+           (x-histogram (first xy-histograms))
+           (y-histogram (second xy-histograms))
+           (x-possibly-circles (wave x-histogram))
+           (y-possibly-circles (wave y-histogram)))
+      (format t "x-possibly-circles: ~A ~%" x-possibly-circles)
+      (format t "y-possibly-circles: ~A ~%" y-possibly-circles)
+      (format t "RESULT ~%~A~%" (generate-circles x-possibly-circles y-possibly-circles)))))
+
 ;; (make-normalised-histograms '((3 3) (3 4) (3 5) (4 4) (10 10)))
 ;; (make-histograms z)
 ;; (defvar ar (make-normalised-histograms z))
@@ -72,7 +154,7 @@ merged into one peak, which placed between them."
 (setf x (first ar))
 (setf y (second ar))
 (find-peaks x)
-(find-peaks y)
+(find-peaks x :min-peak-value 5)
 
 
 
